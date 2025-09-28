30516284cc3d9f625e1992b978078f01f643afb8
Corey Wirun <RotateAt60MPH@users.noreply.github.com>
2025-09-28 08:40:18 -0600

    importusers change

--- src/services/api.js
+++ src/services/api.js
@@ -253,7 +253,7 @@ async function importUsers(
   let url = `${config.API_URL}/auth/importusers`;
   const data = await internetJsonFetch("POST", url, formData, {
     Authorization: `Bearer ${token}`,
-    'Content-Type': 'application/json;charset=UTF-8',
+    'Content-Type': undefined
   });
 
   return data;
